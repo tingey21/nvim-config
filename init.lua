dc1eb05daeb7b39b338479091f1df3ade12a7aa4
Alexander Tingey <alextingey1@gmail.com>
2024-07-23 10:41:14 -0600

    updated config

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("Comment").setup()
+vim.opt.wrap = true
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,35 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", ";", ":", { nowait = true, desc = "enter command mode" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { nowait = true })
 vim.keymap.set("n", "<C-u", "<C-u>zz", { nowait = true })
+vim.keymap.set("n", "{", "{zz", { nowait = true })
+vim.keymap.set("n", "}", "}zz", { nowait = true })
+vim.keymap.set("n", "<TAB>", ":lua GoToNextOrFirstBuffer()<CR>", { desc = "Next Buffer", nowait = true, silent = true })
+
+vim.keymap.set(
+  "n",
+  "<S-TAB>",
+  ":lua GoToPrevOrLastBuffer()<CR>",
+  { desc = "Prev Buffer", nowait = true, silent = true }
+)
+
+function GoToNextOrFirstBuffer()
+  local current_buffer = vim.api.nvim_get_current_buf()
+  local last_buffer = vim.api.nvim_buf_get_number(vim.api.nvim_list_bufs()[#vim.api.nvim_list_bufs()])
+  if current_buffer == last_buffer then
+    vim.cmd("bfirst")
+  else
+    vim.cmd("bnext")
+  end
+end
+
+function GoToPrevOrLastBuffer()
+  local current_buffer = vim.api.nvim_get_current_buf()
+  local first_buffer = vim.api.nvim_buf_get_number(vim.api.nvim_list_bufs()[1])
+  if current_buffer == first_buffer then
+    vim.cmd("blast")
+  else
+    vim.cmd("bprev")
+  end
+end
