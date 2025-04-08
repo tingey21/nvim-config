3e4ff4f604f9ab90434087b90c63670030d74504
Alexander Tingey <alextingey1@gmail.com>
2025-04-08 09:11:17 -0600

    updated plugins

--- init.lua
+++ init.lua
@@ -4,3 +4,6 @@ require("Comment").setup()
 vim.opt.wrap = true
 
 require("focus").setup()
+require("lspconfig").omnisharp.setup({
+  cmd = { "dotnet", "/Users/master/Downloads/omnisharp-osx-arm64-net6.0/OmniSharp.dll" },
+})
